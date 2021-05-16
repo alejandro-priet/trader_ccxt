4ab137228a1a8f6b8477e0a8369e93b704c71d41
alejandro-priet <alejandro.prieto@protonmail.com>
2021-05-16 18:58:54 +0200

    adjust ticker

--- Beta/ccxt_order.py
+++ Beta/ccxt_order.py
@@ -6,24 +6,32 @@ import numpy as np
 i = 0
 y_bid = []
 y_ask = []
+x_data = []
 
-while i < 3: 
-    # retrieve data for the BTC/USDT pair on Binance
-    binance = ccxt.binance()
-    orderbook = binance.fetch_order_book('BTC/USDT')
-    bid = orderbook['bids']
-    ask = orderbook['asks']
+plt.ion()
 
-    for d in range(len(bid)):
-    
-        y_bid.append(bid[d][0]) 
-        y_ask.append(ask[d][0]) 
-    
-    i += 1
+fig = plt.figure()
 
-x_data = np.array(range(len(y_bid)))
+# retrieve data for the BTC/USDT pair on Binance
+binance = ccxt.binance()
+orderbook = binance.fetch_order_book('BTC/EUR')
+ticker = binance.fetch_ticker('BTC/EUR')
+print(ticker)
+bid = orderbook['bids']
+ask = orderbook['asks']
+
+for d in range(len(bid)):
+
+    y_bid.append(bid[d][0]) 
+    y_ask.append(ask[d][0]) 
+
+    plt.plot(y_bid)
+    plt.plot(y_ask)
+    plt.draw()
+    plt.pause(0.0001)
+    plt.clf()
 
 
-fig = plt.plot()
-plt.plot(x_data,y_bid) 
-plt.show()
+
+
+
